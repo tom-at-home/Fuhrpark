ab3fdf6381be1329513e01a4a0a24bb7e09ce60f
Tomasz <tomasz.urbaniak@me.com>
2017-01-27 17:29:19 +0100

    Test 2 - IntelliJ mit Fuhrpark

--- Fuhrpark/src/Fuhrpark/Car.java
+++ Fuhrpark/src/Fuhrpark/Car.java
@@ -2,4 +2,18 @@ package Fuhrpark;
 
 public class Car extends Vehicle{
 
+    protected boolean isEngineOn;
+
+    public Car(String manufacturer, String color, double purchase_price){
+        super(manufacturer, color, purchase_price);
+    }
+
+    public void start_engine(){
+
+    }
+
+    public void stop_engine(){
+
+    }
+
 }
